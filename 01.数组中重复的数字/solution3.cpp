6f89b676b71f215024e997654fb047c85e2c2454
gZeyu <gzy8535@gmail.com>
2019-02-28 14:15:31 +0800

    01 solution3.cpp

--- 01.数组中重复的数字/solution3.cpp
+++ 01.数组中重复的数字/solution3.cpp
@@ -1,23 +1,31 @@
-class Solution {
-public:
+class Solution
+{
+  public:
     // Parameters:
     //        numbers:     an array of integers
     //        length:      the length of array numbers
     //        duplication: (Output) the duplicated number in the array number
     // Return value:       true if the input is valid, and there are some duplications in the array number
     //                     otherwise false
-    bool duplicate(int numbers[], int length, int* duplication) {
-        std:map<int,bool> number_map;
+    bool duplicate(int numbers[], int length, int *duplication)
+    {
         for (int i = 0; i < length; ++i)
         {
-            if (number_map[numbers[i]])
+            if (numbers[i] == i)
+            {
+                continue;
+            }
+            else if (numbers[i] == numbers[numbers[i]])
             {
                 *duplication = numbers[i];
                 return true;
             }
             else
             {
-                number_map[numbers[i]] = true;
+                int tmp;
+                tmp = numbers[numbers[i]];
+                numbers[numbers[i]] = numbers[i];
+                numbers[i] = tmp;
             }
         }
         return false;
